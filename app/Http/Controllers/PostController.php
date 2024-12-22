bc5ce390e23e6cec159e9731a6aa3331cfd65d3d
Hieronimus Pradnya Narendra <hieropradnya77@gmail.com>
2024-12-22 19:33:58 +0700

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostDetailResource;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    function index()
+    {
+        // $posts = Post::all();
+        // return response()->json([
+        //     'data' => $posts
+        // ], 200);
+
+        return PostResource::collection(Post::all());
+    }
+
+    function show($id)
+    {
+        $post = Post::findOrFail($id);
+        return new PostDetailResource($post);
+    }
+}
